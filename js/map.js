e56127efc260d60ed7d628858167dce80dba1aa7
crice937 <xiong937@gmail.com>
2016-11-07 14:28:01 -0500

    Began to attempt implementing mapbox vector tile layer. Layer will eventually be styled to use the dwellingUn property to extrude into a 3D representation of density.

--- js/map.js
+++ js/map.js
@@ -2,29 +2,35 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiY3JpY2U5MzciLCJhIjoiZjhmNmY3NzFiN2ZmODExZmVlMWYyMGZlZWMzNzE1ZDcifQ.3435dWIDUWFnAa6oicXfRQ';
 
 var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/outdoors-v10',
-    center: [-84.2,33.85],
-    zoom: 12
+	container: 'map',
+	style: 'mapbox://styles/mapbox/outdoors-v10',
+	center: [-84.2,33.85],
+	zoom: 12
 });
 
 map.on('load', function () {
-    map.addSource('parcel_dwellingUnit-3mizhu', {
-        type: 'vector',
-        url: 'mapbox://crice937.5sm2dk25'
-    });
-    map.addLayer({
-        "id": "terrain-data",
-        "type": "line",
-        "source": "terrain-data",
-        "source-layer": "contour",
-        "layout": {
-            "line-join": "round",
-            "line-cap": "round"
-        },
-        "paint": {
-            "line-color": "#ff69b4",
-            "line-width": 1
-        }
-    });
+	map.addSource('duDensity', {
+		type: 'vector',
+		url: 'mapbox://crice937.5sm2dk25'
+	});
+	map.addLayer({
+		"id": "crice937.5sm2dk25",
+		"type": "vector",
+		"source": "duDensity",
+		"source-layer": "parcel_dwellingUnit-3mizhu",
+		'filter': ['==', 'extrude', 'true'],
+        'type': 'fill',
+		'paint': {
+			'fill-color': '#aaa',
+			'fill-extrude-height': {
+				'type': 'identity',
+				'property': 'dwellingUn'
+			},
+            'fill-extrude-base': {
+                'type': 'identity',
+                'property': 'dwellingUn'
+            },
+			'fill-opacity': .6
+		}
+	});
 });
